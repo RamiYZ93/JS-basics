d4aac61cd27bd6a5ab49edabf62e6a584ac9180d
Rami <touati.rami.19@gmail.com>
2026-02-20 21:39:22 +0100

    Add TD01-variables

---
+++ TD01-variables/script.js
@@ -0,0 +1,35 @@
+//1
+let chienAnimal = "Rex";
+
+// 2
+let chatAnimal = "Pepper";
+
+// 3
+console.log(chienAnimal);
+
+// 4
+console.log(chatAnimal);
+
+// 5
+console.log("Le nom de mon chien est : " + chienAnimal);
+
+// 6
+console.log("Le nom de mon chat est : " + chatAnimal);
+
+// 7
+let sonChat = "ronron";
+
+// 8
+let sonChien = "ouaf";
+
+// 9
+console.log(chienAnimal + " dit " + sonChien);
+
+// 10
+console.log(chatAnimal + " dit " + sonChat);
+
+// 11
+sonChat = "miaou";
+
+// 12
+console.log(chatAnimal + " dit maintenant " + sonChat);
